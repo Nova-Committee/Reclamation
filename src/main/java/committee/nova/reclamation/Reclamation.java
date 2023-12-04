368a0702e5dd8307165db68744e726a905268528
MikhailTapio <mikhailtapio13468@163.com>
2023-12-04 16:16:41 +0800

    Add displaytest for 1.18.2

---
+++ src/main/java/committee/nova/reclamation/Reclamation.java
@@ -0,0 +1,13 @@
+package committee.nova.reclamation;
+
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("reclamation")
+public class Reclamation {
+    public Reclamation() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> "", (a, b) -> true));
+    }
+}
